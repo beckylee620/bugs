4b8a8f0f0e5ceed75501028abbdb7e83d01cdaa4
becky <6534463+beckylee620@users.noreply.github.com>
2020-01-12 16:34:25 +0000

    checking for floating 1s; converting for loop check to while loop

--- generate_bugs.py
+++ generate_bugs.py
@@ -17,27 +17,33 @@ unique_pixels = half_width * length
 for i in itertools.product([0,1],repeat=unique_pixels):
 # make an array
     matrix = np.reshape(np.array(i),(length,half_width))
-    valid = True
+    valid,bug = True,False
+    while valid:
 # check that every row and every column have a 1
-    for x in range(0,half_width):
-        if not 1 in matrix[:,x]:
-            valid = False
-    for y in range(0,length):
-        if not 1 in matrix[y,:]:
-            valid = False
-# check for every possible free-floating 1????
+        for x in range(0,half_width):
+            if not 1 in matrix[:,x]:
+                valid = False
+        for y in range(0,length):
+            if not 1 in matrix[y,:]:
+                valid = False
+# check that there are no free-floating 1s
 # and that there is exactly one eye
-    if valid:
         eyes = 0
         for x in range(0,half_width-2):
             for y in range(0,length-2):
-#                print(matrix[y:y+3,x:x+3])
                 if np.array_equal(matrix[y:y+3,x:x+3],np.array([[1,1,1],[1,0,1],[1,1,1]])):
                     eyes += 1
+                if np.array_equal(matrix[y:y+3,x:x+3],np.array([[0,0,0],[0,1,0],[0,0,0]])):
+                    valid = False
+#!! look along edges and middle for lonely 1s
+#!! and look along middle for extra eyes
         if eyes != 1:
             valid = False
+        if valid:
+            bug = True
+            valid = False
 # when a valid bug is found:
-# print matrix number by converting to binary
-# print nicely-formatted reflected matrix (accounting for even numbers of columns)
-    if valid:
+#!! print matrix number by converting to binary
+#!! print nicely-formatted reflected matrix (accounting for even numbers of columns)
+    if bug:
         print(matrix)
